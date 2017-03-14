86fe3952c36af7070245ee34326d4977fe449b17
SujKumar <kumar@MacBook-Pro.local>
2017-03-14 09:51:34 +0000

    Shopping cart demo commit

---
+++ src/main/java/com/example/shopping/ShoppingCartDemoAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartDemoAppApplication.class, args);
+	}
+}
